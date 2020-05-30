a6becb853816d84b111170326cac1b41cc010b4d
Rahul Dhyani <Rahul.Dhyani01@yahoo.com>
2020-05-31 06:04:05 +1000

    comment changes to src and test file

--- Working/Project/src/app.js
+++ Working/Project/src/app.js
@@ -1,4 +1,4 @@
-console.log("My first Java Script");
+console.log("My first Java Script source code");
 function app(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
--- Working/Project/test/app.test.js
+++ Working/Project/test/app.test.js
@@ -1,4 +1,4 @@
-console.log("Test File");
+console.log("My first Jest File");
 const app = require ('../src/app.js');
 test('multiplies 2 and 3 to be equal to 6', () => {
   expect(app(2, 3)).toBe(6);
