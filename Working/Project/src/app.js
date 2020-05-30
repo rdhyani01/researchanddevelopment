f621354266d265c4d2cd5f0d9a1e2dafa97d12c9
Rahul Dhyani <Rahul.Dhyani01@yahoo.com>
2020-05-31 07:34:32 +1000

    updated console log for src

--- Working/Project/src/app.js
+++ Working/Project/src/app.js
@@ -1,4 +1,4 @@
-console.log("My first Java Script source code");
+console.log("My first Java Script source code updated to test Jenkins-Github interaction");
 function app(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
