ecb7ace10ba33960e476e5bacdc5dc5d2b1767bd
Rahul Dhyani <Rahul.Dhyani01@yahoo.com>
2020-06-07 10:52:46 +1000

    changes to app.test.js, added Dockerfile and package-lock.json

--- Working/Project/test/app.test.js
+++ Working/Project/test/app.test.js
@@ -1,4 +1,4 @@
-console.log("My first Jest Unit Test File");
+console.log("My first Jest Unit Test File for Jenkins & Github integration");
 const app = require ('../src/app.js');
 test('multiplies 2 and 3 to be equal to 6', () => {
   expect(app(2, 3)).toBe(6);
